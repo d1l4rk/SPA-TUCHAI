cfe6956c6975b6040336df1ba97fb76b3a506ffa
d1l4rk <mproducern70@gmail.com>
2021-02-17 23:28:03 -0500

    lalala

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-$(function(){
-  $('a[href^="#"]:not([href="#"])').click(function(){
-    $("html, body").animate({
-      //IMPORTANT: One extra pixel to pass the line!
-      scrollTop: $($(this).attr("href")).position().top + 1
-    }, 500);
-    return false;
-  });
-});
-
-//Set line width and position:
 function setLine(tab){
   var tabWidth = $(tab).outerWidth();
   var tabPos = $(tab).position();
@@ -18,11 +7,10 @@ function setLine(tab){
   });
 };
 
-//Line move:
 function lineMove(){
   $('.tab').each(function (){
     var scrollPos = $(document).scrollTop();
-    var linkRef = $($(this).attr("href"));
+    var linkRef = $($(this).attr("data-attr-scroll"));
     var refPos = linkRef.position().top;
     var refHeight = linkRef.height();
     if(refPos <= scrollPos && refPos + refHeight > scrollPos){
@@ -32,6 +20,36 @@ function lineMove(){
     };
   });
 };
+
+jQuery(document).ready(function($) {
+  $(".tab").click(function(event) {
+      event.preventDefault(); 
+
+      var defaultAnchorOffset = 0;
+
+      var anchor = $(this).attr('data-attr-scroll');
+
+      var anchorOffset = $(anchor).attr('data-scroll-offset');
+      if (!anchorOffset)
+          anchorOffset = defaultAnchorOffset; 
+
+      // $('html,body').animate({ 
+      //     scrollTop: $(anchor).offset().top - anchorOffset
+      // }, 500);        
+  });
+});
+$(function(){
+  $('a[data-attr-scroll^="#"]:not([data-attr-scroll="#"])').click(function(){
+    $("html, body").animate({
+      //IMPORTANT: One extra pixel to pass the line!
+      scrollTop: $($(this).attr("data-attr-scroll")).position().top + 1
+    }, 650);
+    return false;
+  });
+});
+
+
+
 
 //Run function:
 $(document).ready(function(){
